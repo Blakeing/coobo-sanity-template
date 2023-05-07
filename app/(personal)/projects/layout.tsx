8e8826cf07329b3f7ab76b4e7a3c786029b77729
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-07 16:45:10 -0500

    Create layout.tsx

---
+++ app/(personal)/projects/layout.tsx
@@ -0,0 +1,27 @@
+import type { PortableTextBlock } from '@portabletext/types'
+import { Footer } from 'components/global/Footer'
+import { Navbar } from 'components/global/Navbar'
+import { PreviewBanner } from 'components/preview/PreviewBanner'
+import { getSettings } from 'lib/sanity.client'
+import { getPreviewToken } from 'lib/sanity.server.preview'
+
+export default async function IndexRoute({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const token = getPreviewToken()
+  const settings = (await getSettings({ token })) || {
+    menuItems: [],
+    footer: [],
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col bg-white text-black">
+      {token && <PreviewBanner />}
+      <Navbar menuItems={settings.menuItems} />
+      <div className="mt-20 grow px-4 md:px-16 lg:px-32">{children}</div>
+      <Footer footer={settings.footer as PortableTextBlock[]} />
+    </div>
+  )
+}
