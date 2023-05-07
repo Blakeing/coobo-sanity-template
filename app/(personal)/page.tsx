e6ba11273661d7c49ec43f279fa2bfe2f7964395
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-07 16:43:18 -0500

    update scroll

--- app/(personal)/page.tsx
+++ app/(personal)/page.tsx
@@ -1,9 +1,5 @@
 import Header from '@/components/header'
 import HomeSlider from '@/components/home-slider'
-import { SiteHeader } from '@/components/site-header'
-import { buttonVariants } from '@/components/ui/button'
-import { siteConfig } from '@/config/site'
-import Link from 'next/link'
 
 export default function HomePage() {
   return (
